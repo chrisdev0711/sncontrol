260c53bdfbf205599aee38690b5d265d55be70e6
PandaDevA <mianhua9999@gmail.com>
2022-01-17 12:38:15 +0100

    add sleep time for 10sec

--- app/Http/Controllers/Central/RegisterTenantController.php
+++ app/Http/Controllers/Central/RegisterTenantController.php
@@ -91,7 +91,7 @@ class RegisterTenantController extends Controller
             $name = 'snappyio/stocknow',
         );
 
-        sleep(6);
+        sleep(10);
 
         // Create Certificate
         $ploi->servers($serverId)->sites($siteId)->certificates()->create(
@@ -126,7 +126,7 @@ class RegisterTenantController extends Controller
         //     $serverIds = [$serverId],
         // );
 
-        sleep(6);
+        sleep(10);
 
         // Development
         $deployment = "cd /home/ploi/{$siteDomain}\ngit pull origin main\ncomposer install --no-interaction --prefer-dist --optimize-autoloader\nphp artisan key:generate\nphp artisan config:clear\nphp artisan cache:clear\nphp artisan route:cache\nphp artisan view:clear\nphp artisan migrate --force\nphp artisan db:seed --force\necho \"\" | sudo -S service php7.4-fpm reload\necho \"🚀 Application deployed!\"";
